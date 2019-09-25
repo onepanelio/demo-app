bfa382efbd7e7b4377166d5f062524fecd8e049e
nadir <nadirlaskar@gmail.com>
2019-09-26 04:58:57 +0530

    updated loader code

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,1 +1,23 @@
-export default ({ loading }) => `${loading}`;
+import React from 'react';
+import {
+  Text,
+  Spinner,
+  View
+} from 'native-base';
+
+export default ({ loading }) => (
+  <View
+    style={{
+      flex: 1,
+      position: 'absolute',
+      zIndex: 110,
+      top: '50%',
+      alignSelf: 'center',
+    }}
+  >
+    <Spinner color="blue" />
+    <Text style={{ color: 'white' }}>
+      {`Finding ${loading ? '...' : 'Done'}`}
+    </Text>
+  </View>
+);
